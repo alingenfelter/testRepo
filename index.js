54a5ebab83ece72b2da126078ba03063b0eaed76
Andrea <alingenfelter2003@gmail.com>
2016-10-17 10:03:17 -0400

    changes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-console.log("This is a test repo created for learning git hub")
-console.log("This is a change to the feature branch")
-console.log("another change to filter")
+console.log('This is a test repo created for learning git hub')
+console.log('This is a change to the feature branch')
+console.log('another change to filter')
+
+console.log('doing changes to master branch to be commited locally.')
+console.log('doing changes to feature branch to be commited and merged locally')
